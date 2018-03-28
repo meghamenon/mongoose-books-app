86cab17786431717c22ef9da3cc8f5a1c9fb97c4
meghamenon <meg04192012@gmail.com>
2018-03-28 15:39:41 -0700

    setup sprint-1

--- models/index.js
+++ models/index.js
@@ -1,2 +1,15 @@
+//This is connecting to the database
+
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/book-app");
+mongoose.connect("mongodb://localhost/book-app"); 
+
+// this requires the book.js file. like exporting. 
+
+var BookModel = require('./book.js');
+
+module.exports = {
+
+	Book: BookModel,
+
+}
+
